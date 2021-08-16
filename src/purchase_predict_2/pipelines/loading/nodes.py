3cd23955d9c7914e674521c39c5f2a24e04a6286
Samanth Chinta <samanth.chinta@gmail.com>
2021-08-16 13:34:24 +0200

    change loading node

--- src/purchase_predict_2/pipelines/loading/nodes.py
+++ src/purchase_predict_2/pipelines/loading/nodes.py
@@ -8,7 +8,6 @@ def load_csv_from_bucket(project: str, bucket_path: str) -> pd.DataFrame:
     """
     Loads multiple CSV files from bucket (as generated from Spark).
     """
-    d = r"C:\Users\saman\AppData\Local\Temp\\"
     storage_client = storage.Client()
     bucket_name = bucket_path.split("/")[0]
     folder = "/".join(bucket_path.split("/")[1:]) + "/part-"
@@ -16,9 +15,9 @@ def load_csv_from_bucket(project: str, bucket_path: str) -> pd.DataFrame:
     for blob in storage_client.list_blobs(bucket_name, prefix=folder):
         filename = blob.name.split("/")[-1]
         if filename[-3:] == "csv":
-            blob.download_to_filename(d + filename)
+            blob.download_to_filename("/tmp/" + filename)
 
-    all_files = glob.glob(d + "*.csv")
+    all_files = glob.glob("/tmp/*.csv")
     li = []
 
     for filename in all_files:
